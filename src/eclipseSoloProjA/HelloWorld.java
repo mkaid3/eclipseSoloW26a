1128cadfd464efa52f79ca4224290025edc0b968
mkaid3 <mkaid3@emich.edu>
2026-02-15 00:59:03 -0500

    Did some nifty stuff

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -3,7 +3,8 @@ package eclipseSoloProjA;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Howdy github Project");
+		System.out.println("Hello World");
 
 	}
 
