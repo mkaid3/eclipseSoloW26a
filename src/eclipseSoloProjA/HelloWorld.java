b2b397745c8d58d44298c3344c7fba684f95e022
mkaid3 <mkaid3@emich.edu>
2026-02-15 02:02:09 -0500

    ProgrammerOne

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Howdy github Project");
 		System.out.println("Hello World");
-		System.out.println("Programmer 2 did this");
+		System.out.println("ProgrammerOne did this");
 
 	}
 
